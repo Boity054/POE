4c65c3b1fff438219aabced41751e4cb565b45f2
RC_Student_lab <RC_Student_lab@10.0.0.63>
2024-09-30 16:54:00 +0000

    testing

--- src/main/java/com/mycompany/poe_part1/Login.java
+++ src/main/java/com/mycompany/poe_part1/Login.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.poe_part1;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 /**
  *
  * @author RC_Student_lab
@@ -13,6 +14,8 @@ public class Login {
     private String lastname;
     private String username;
     private String password;
+    private String tempUsername;
+    private String tempPassword;
     
     //Getters and Setters
     public String getFirstname(){
@@ -34,22 +37,50 @@ public class Login {
         this.username=username;
     }
     public String getPassword(){
-        return username;
+        return password;
     }
     public void setPassword(String password){
         this.password=password;
     }
+     //method to check the username 
+    public boolean checkUsername(String username){
+         //check if the user name meets the conditions
+         return !username.contains("_") && 
+                username.length()>=5;
+    }
     
+    //method to check for password
+    public boolean checkPasswordComplexity(String password){
+        //check if the password is complex  
+       return password.length() >= 8 &&
+              password.matches(".*[A-Z].*") &&
+              password.matches(".*[0-9].*") &&
+              password.matches(".*[!@#$%^&*().]*");
+
+    }
+      //method to register the user
+    public String registerUser(String username, String password){
+        if(!checkUsername(username)){
+            return "The username is incorrectly formatted.";
+        }
+        else if(!checkPasswordComplexity(password)){
+            return "The password does not meet the complexity requirements.";
+        }
+        else{
+            tempUsername = username;
+            tempPassword = password;
+            return "The two above conditions have been met and the user has been registered successfully.";
+        }
+    }
+     
     //method to verify the login details
     public static boolean loginUser(String username, String password){
-        do{
         if(username.equals(username) && password.equals(password)){
-           return true;
+            return true;
         }
         else{
             return false;
-        } 
-        }while(false);
+        }
     }
     
     public String returnLoginStatus(boolean successfulLogin){
@@ -58,40 +89,6 @@ public class Login {
         }
         else{
             return "Username or password incorrect, try again.";
-        }
-    }
-
-     //method to check the username 
-    public static boolean checkUsername(String username){
-         //check if the user name meets the conditions
-         
-         if (username.equals(username.contains("_") && username.length()<=5)){
-             System.out.println("Username successfully captured.");
-         }
-         else{
-            System.out.println("Username is not correctly formatted, please ensure that your username contains an underscore is no more than 5 characters in length.");
-         }
-         return username.contains("_") && username.length()<=5;
-    }
-    
-    //method to check for password
-    public static boolean checkPasswordComplexity(String password){
-        //check if the password is complex
-        if(password.equals(password.length()<=8 && password.matches("[A-Z]") && password.matches("[0-9]") && password.contains("~!@#$%^&*()_-?/<>:}{]["))){
-            System.out.println("Password successfully captured.");
-        }
-        else{
-              System.out.println("Password is not correctly formatted, please ensure that the password contains at least 8 character,a capital letter, a number and a speacial charater.");
-        }   
-        return password.length()<=8 && password.matches("[A-Z]") && password.matches("[0-9]") && password.contains("~!@#$%^&*()_-?/<>:}{][");
-    }
-      //method to register the user
-    public static String registerUser(String username, String password){
-        if(!checkUsername(username) && !checkPasswordComplexity(password)){
-            return "The username is incorrectly formatted or The password does not meet the complexity requirements.";
-        }
-        else{
-            return "The two above conditions have been met and the user has been registered successfully.";
         }
     }
 }   
---
+++ src/test/java/com/mycompany/poe_part1/LoginTest.java
@@ -0,0 +1,199 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.poe_part1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class LoginTest {
+    @Test
+    public void testGetFirstname() {
+        System.out.println("getFirstname");
+        Login instance = new Login();
+        String expResult = "";
+        String result = instance.getFirstname();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of setFirstname method, of class Login.
+     */
+    @Test
+    public void testSetFirstname() {
+        System.out.println("setFirstname");
+        String firstname = "";
+        Login instance = new Login();
+        instance.setFirstname(firstname);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of getLastname method, of class Login.
+     */
+    @Test
+    public void testGetLastname() {
+        System.out.println("getLastname");
+        Login instance = new Login();
+        String expResult = "";
+        String result = instance.getLastname();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+
+    /**
+     * Test of setLastname method, of class Login.
+     */
+    @Test
+    public void testSetLastname() {
+        System.out.println("setLastname");
+        String lastname = "";
+        Login instance = new Login();
+        instance.setLastname(lastname);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+
+    /**
+     * Test of getUsername method, of class Login.
+     */
+    @Test
+    public void testGetUsername() {
+        System.out.println("getUsername");
+        Login instance = new Login();
+        String expResult = "";
+        String result = instance.getUsername();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+
+    /**
+     * Test of setUsername method, of class Login.
+     */
+    @Test
+    public void testSetUsername() {
+        System.out.println("setUsername");
+        String username = "kyl_1";
+        Login instance = new Login();
+        instance.setUsername(username);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of getPassword method, of class Login.
+     */
+    @Test
+    public void testGetPassword() {
+        System.out.println("getPassword");
+        Login instance = new Login();
+        String expResult ="";
+        String result = instance.getPassword();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of setPassword method, of class Login.
+     */
+    @Test
+    public void testSetPassword() {
+        System.out.println("setPassword");
+        String password = "Ch&&sec@ke99!";
+        Login instance = new Login();
+        instance.setPassword(password);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+
+    /**
+     * Test of checkUsername method, of class Login.
+     */
+    @Test
+    public void testCheckUsername() {
+        System.out.println("checkUsername");
+        String username = "kyl_1";
+        Login instance = new Login();
+        boolean expResult = false;
+        boolean result = instance.checkUsername(username);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+
+    /**
+     * Test of checkPasswordComplexity method, of class Login.
+     */
+    @Test
+    public void testCheckPasswordComplexity() {
+        System.out.println("checkPasswordComplexity");
+        String password = "Ch&&sec@ke99!";
+        Login instance = new Login();
+        boolean expResult = true;
+        boolean result = instance.checkPasswordComplexity(password);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of registerUser method, of class Login.
+     */
+    @Test
+    public void testRegisterUser() {
+        System.out.println("registerUser");
+        String username = "kyl_1";
+        String password = "Ch&&sec@ke99!";
+        Login instance = new Login();
+        String expResult = "";
+        String result = instance.registerUser(username, password);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+
+    /**
+     * Test of loginUser method, of class Login.
+     */
+    @Test
+    public void testLoginUser() {
+        System.out.println("loginUser");
+        String username = "kyl_1";
+        String password = "Ch&&sec@ke99!";
+        boolean expResult = true;
+        boolean result = Login.loginUser(username, password);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    }
+
+    /**
+     * Test of returnLoginStatus method, of class Login.
+     */
+    @Test
+    public void testReturnLoginStatus() {
+        System.out.println("returnLoginStatus");
+        boolean successfulLogin = false;
+        Login instance = new Login();
+        String expResult = "";
+        String result = instance.returnLoginStatus(successfulLogin);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+      
+    }
+    
+}
